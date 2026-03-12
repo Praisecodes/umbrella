bb853ecbf1d7308b9af13644fb78d8ee87147c40
Praisecodes <cmancoder@gmail.com>
2026-03-12 15:52:56 +0100

    chore: setup tailwind colors

---
+++ src/app/(auth)/login.tsx
@@ -0,0 +1,121 @@
+import { Button, HText, Input, Text } from "@/src/components/common";
+import { LOGIN_SCHEMA } from "@/src/helpers/schemas";
+import { getMetrics, handleFormTextChange, validateForm } from "@/src/helpers/utils";
+import AuthLayout from "@/src/layouts/auth";
+import { authService } from "@/src/services";
+import { router } from "expo-router";
+import { useState } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { toast } from "sonner-native";
+import { InferType } from "yup";
+
+type ILoginSchema = InferType<typeof LOGIN_SCHEMA>;
+
+export default function Login() {
+  const [payload, setPayload] = useState<ILoginSchema>({
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleForgotPassword = async () => { }
+
+  const handleLogin = async () => {
+    setLoading(true);
+
+    try {
+      await validateForm(LOGIN_SCHEMA, payload,
+        async () => {
+          const { data, error } = await authService.login(payload);
+
+          if (error) {
+            switch (error.code) {
+              case "email_not_confirmed":
+                router.navigate({
+                  pathname: "/otp",
+                  params: { email: payload.email }
+                });
+                break;
+              default:
+
+            }
+            toast.error(error.message);
+            return;
+          }
+
+          console.log("Data from sign-in:", JSON.stringify(data, null, 2));
+        },
+      )
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const handleCreateAccount = () => {
+    router.navigate("/(auth)/signup");
+  }
+
+  return (
+    <AuthLayout>
+      <View style={styles.container} className={`flex-1`}>
+        <HText
+          className={`text-white capitalize text-center`}
+          size="header1"
+        >
+          login to your account
+        </HText>
+
+        <View style={styles.formContainer}>
+          <Input
+            label="Email Address"
+            placeholder="Enter your email address"
+            value={payload.email}
+            onChangeText={(e) => handleFormTextChange(setPayload, "email", e)}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+
+          <Input
+            label="Password"
+            placeholder="Enter your password"
+            secureTextEntry
+            value={payload.password}
+            onChangeText={(e) => handleFormTextChange(setPayload, "password", e)}
+          />
+
+          <View className={`flex items-end`}>
+            <TouchableOpacity onPress={handleForgotPassword}>
+              <Text size="15" className={`text-[#00ff00] underline`}>
+                Forgot Password
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={{ gap: getMetrics(15) }}>
+          <Button
+            text="LOG IN"
+            onPress={handleLogin}
+            loading={loading}
+          />
+
+          <TouchableOpacity onPress={handleCreateAccount}>
+            <Text className={`text-white text-center`} size="15">
+              Don't have an account? <Text size="15" className={`text-[#00ff00]`}>Create One</Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </AuthLayout>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    gap: getMetrics(45),
+    paddingTop: getMetrics(25),
+  },
+  formContainer: {
+    gap: getMetrics(15),
+  }
+})
